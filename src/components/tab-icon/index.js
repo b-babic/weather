d6d2ef61e6fd99593b6a6f4fc7e307c229b01f45
Benjamin <kakosi69@gmail.com>
2019-09-24 04:02:20 +0200

    Updates on the theme

--- src/components/tab-icon/index.js
+++ src/components/tab-icon/index.js
@@ -1,30 +1,15 @@
 import React, {Component} from 'react';
-// Components
-import {IconWithBadge} from 'components';
-// Hocs
-import {withTheme} from 'hocs';
 // Icons
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 class TabIcon extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    const isHomeScreen = icon == 'weather-hail';
-    const {icon, theme, focused} = this.props;
-    const tintColor = focused
-      ? theme.colors.activeText
-      : theme.colors.inactiveText;
-    if (!isHomeScreen) {
-      return <MaterialCommunityIcon color={tintColor} size={24} name={icon} />;
-    } else {
-    }
-    return (
-      <IconWithBadge
-        MainIcon={
-          <MaterialCommunityIcon name={icon} size={24} color={tintColor} />
-        }
-      />
-    );
+    const {icon, tintColor} = this.props;
+    return <MaterialCommunityIcon color={tintColor} size={24} name={icon} />;
   }
 }
 
-export default withTheme(TabIcon);
+export default TabIcon;
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Scene, Router} from 'react-native-router-flux';
+import {Scene, Router, Actions} from 'react-native-router-flux';
 // Screens
 import Home from './home';
 import Cities from './city';
@@ -8,7 +8,9 @@ import Settings from './settings';
 // Store
 import {observer, inject} from 'mobx-react';
 // Components
-import {TabIcon} from 'components';
+import {TabIcon, BottomNavigation} from 'components';
+// Primitives
+import {Box, Text, Touchable} from 'primitives';
 // ThemeProvider
 import theme from 'theme/theme';
 import {ThemeProvider} from 'theme/context';
@@ -25,7 +27,11 @@ class Screens extends React.Component {
           <Router>
             <Scene key="root" hideNavBar>
               {/* Tab Container */}
-              <Scene key="tabBar" tabBarPosition="bottom" tabs={true}>
+              <Scene
+                key="tabBar"
+                tabBarPosition="bottom"
+                tabs={true}
+                tabBarComponent={BottomNavigation}>
                 <Scene
                   key="home"
                   component={Home}
